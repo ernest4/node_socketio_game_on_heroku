ac77e3cd6c9a229bba9d93458c8fc494dd30a4a0
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-12-15 00:36:59 +0000

    playing around with local dynamodb

--- server.js
+++ server.js
@@ -3,17 +3,222 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-/* Normally this would be persisted in a database...but for now, just store it 
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000",
+    //DO NOT EXPOSE CREDENTIALS ON GIT!!!, use environmental variables set up through heroku dashboard!
+    accessKeyId: process.env.S3_KEY || "abcde",
+    secretAccessKey: process.env.S3_SECRET || "abcde"
+});
+
+var dynamodb = new AWS.DynamoDB();
+
+// --- Initialize DB with tables and shemas
+var params = {
+    TableName: "matchData",
+    KeySchema: [
+        {AttributeName: "matchItem", KeyType: "HASH"}, //parition key (primary key, mandatory)
+        //{AttributeName: "title", KeyType: "RANGE"} //sort key (secondary index, optional)
+    ],
+    AttributeDefinitions: [
+        {AttributeName: "matchItem", AttributeType: "S"}, //number
+        //{AttributeName: "year", AttributeType: "N"}, //number
+        //{AttributeName: "title", AttributeType: "S"} //string
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+    }
+};
+
+dynamodb.createTable(params, function(err, data){
+    if (err) console.log("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    else console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+});
+
+/*dynamodb.deleteTable({TableName: "Movies"}, function(err, data) {
+    if (err) {
+        console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Deleted table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});*/
+
+
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+
+// --- creating the initial items
+var params1 = {
+    TableName: "matchData",
+    Item: {
+        matchItem: "star",
+        info: {
+            x: Math.floor(Math.random() * 700) + 50,
+            y: Math.floor(Math.random() * 500) + 50
+        }
+    },
+    //only add the item if it doesn't already exist, i.e. if Exists: false ===> true...
+    Expected: {
+        matchItem: {
+            Exists: false,
+            Value: "star"
+        }
+    },
+};
+
+docClient.put(params1, function(err, data){
+    if (err) console.error("Unable to add star item. Error JSON:", JSON.stringify(err, null, 2));
+    else console.log("Added item:", JSON.stringify(data, null, 2));
+});
+
+var params2 = {
+    TableName: "matchData",
+    Item: {
+        matchItem: "scores",
+        info: {
+            blue: 0,
+            red: 0
+        }
+    },
+    //only add the item if it doesn't already exist, i.e. if Exists: false ===> true...
+    Expected: {
+        matchItem: {
+            Exists: false,
+            Value: "scores"
+        }
+    },
+}
+
+docClient.put(params2, function(err, data){
+    if (err) console.error("Unable to add scores item. Error JSON:", JSON.stringify(err, null, 2));
+    else console.log("Added item:", JSON.stringify(data, null, 2));
+});
+
+
+
+
+// --- Reading
+function getMatchData(matchItem, socket){
+    let params = {
+        TableName: "matchData",
+        Key: {
+            "matchItem": matchItem
+        }
+    };
+    
+    docClient.get(params, function(err, data){
+        if (err) console.error(`Unable to read ${matchItem} item. Error JSON: ${JSON.stringify(err, null, 2)}`);
+        else { 
+            console.log(`GetItem succeeded: ${JSON.stringify(data, null, 2)}`);
+            //console.log(`Got matchData: ${data.Item.info}`);
+            if (matchItem === "star"){
+                socket.emit('starLocation', data.Item.info); //update player
+                star = (star == null) ? data.Item.info : star; //update global state (if it's the first time)
+                //console.log("STAR::"+data.Item.info);
+                //NOTE: (... == null) will catch both null and undefined in one test!
+            } else {
+                socket.emit('scoreUpdate', data.Item.info);
+                scores = (scores == null) ? data.Item.info : scores; //update global state (if it's the first time)
+            }
+        }
+    });
+}
+
+
+function saveMatchData(matchItem){
+    if (matchItem === "star") {
+        // --- update the items
+        var params = {
+            TableName: "matchData",
+            Key:{
+                matchItem: "star"
+            },
+            //UpdateExpression: "set info.x = :x, info.y = :y, info.test = :t",
+            UpdateExpression: "set info.x = :x, info.y = :y",
+            ExpressionAttributeValues:{
+                ":x": +star.x, //update old data
+                ":y": +star.y, //update old data
+                //":t": ["Larry", "Moe", "Curly"] //add some new data..
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+    } else { //scores
+        var params = {
+            TableName: "matchData",
+            Key:{
+                matchItem: 'scores'
+            },
+            UpdateExpression: "set info.blue = :blue, info.red = :red",
+            ExpressionAttributeValues: {
+                ":blue": +scores.blue,
+                ":red" : +scores.red
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+    }
+
+    docClient.update(params, function(err, data) {
+        if (err) console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+        else console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+    });
+}
+
+/*
+// Conditional delete
+var params6 = {
+    TableName: "matchData",
+    Key:{
+        matchItem: 'scores'
+    },
+    UpdateExpression: "remove info.test[0]",
+    ConditionExpression: "size(info.test) >= :val",
+    ExpressionAttributeValues: {
+        ":val": 3
+    },
+    ReturnValues:"UPDATED_NEW"
+};
+
+docClient.update(params6, function(err, data) {
+    if (err) console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    else console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+});
+*/
+
+
+
+/* Untill this is persisted in a database, just store it 
 locally on the server. */
-var players = {};
-var star = {
+/*var star = {
     x: Math.floor(Math.random() * 700) + 50,
     y: Math.floor(Math.random() * 500) + 50
 };
 var scores = {
     blue: 0,
     red: 0
-};
+};*/
+
+//Using dynamoDB
+var star;
+var scores;
+
+//periodically back up the score to database...
+setInterval(function(){
+    if (star == null || scores == null) return; //not ready for saving yet...
+
+    io.emit('savedToDB', `Saved the data to database... BLUE: ${scores.blue} RED: ${scores.red} Time: ${new Date().toTimeString()}`);
+
+    saveMatchData('star');
+    saveMatchData('scores');
+
+}, 3000);
+
+
+
+var players = {};
 
 const PORT = process.env.PORT || 3000;
 
@@ -38,10 +243,14 @@ io.on('connection', function (socket){
     //Update the new player of the current game state...
     //send the players objects to new player
     socket.emit('currentPlayers', players);
+
     //send the star object to the new player
-    socket.emit('starLocation', star);
+    //socket.emit('starLocation', star);
+    getMatchData("star", socket);
+
     //send the current scores
-    socket.emit('scoreUpdate', scores);
+    //socket.emit('scoreUpdate', scores);
+    getMatchData("scores", socket);
 
     //update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
@@ -66,6 +275,7 @@ io.on('connection', function (socket){
         players[socket.id].x = movementData.x;
         players[socket.id].y = movementData.y;
         players[socket.id].rotation = movementData.rotation;
+
 
         //emit message to all players about the player that moved
             //socket.broadcast.emit('playerMoved', players[socket.id]);
